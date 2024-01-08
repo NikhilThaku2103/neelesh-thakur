87892b476f1d355fd51ad374319c2897b247a646
suzy311278 <narbhaxsh@gmail.com>
2024-01-08 22:01:46 +0530

    d

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="profile-image-container">
+        <img src="your-profile-image.jpg" alt="Your Name" className="profile-image" />
+      </div>
+      <h1>Neelesh Thakur</h1>
+      <p>Entrepreneur</p>
+    </header>
+  );
+}
+export default Header;
